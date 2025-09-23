6b491064bcc093629513e0cf278e425a26e750ee
Sadrtdinov <nailsadrt8927@gmail.com>
2025-09-23 16:50:01 +0400

    Добавлен класс SqrFunction и метод apply возвращающий квадрат аргуменита

--- src/main/java/ru/ssau/tk/faible/labs/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/faible/labs/functions/MathFunction.java
@@ -2,4 +2,5 @@ package ru.ssau.tk.faible.labs.functions;
 
 interface MathFunction {
     double apply(double x);
+    
 }
---
+++ src/main/java/ru/ssau/tk/faible/labs/functions/SqrFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.faible.labs.functions;
+
+public class SqrFunction implements MathFunction{
+    public double apply(double x){
+        return Math.pow(x,2);
+    }
+}
