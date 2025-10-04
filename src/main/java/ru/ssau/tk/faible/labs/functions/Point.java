ce6aa5f91e924da47b098817ca695ea6d284d654
Sadrtdinov <nailsadrt8927@gmail.com>
2025-10-04 14:28:33 +0400

    Добавлен класс Point

---
+++ src/main/java/ru/ssau/tk/faible/labs/functions/Point.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.faible.labs.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public Point(double dotx, double doty){
+        x = dotx;
+        y = doty;
+    }
+}
