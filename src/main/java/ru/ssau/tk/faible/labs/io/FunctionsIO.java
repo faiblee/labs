e69c73570971e3585468659f106c1c65276b8485
Sadrtdinov <nailsadrt8927@gmail.com>
2025-10-11 17:46:01 +0400

    В классе FunctionsIO добавить статический метод: void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function),в пакете io создан класс TabulatedFunctionFileWriter

--- src/main/java/ru/ssau/tk/faible/labs/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/faible/labs/io/FunctionsIO.java
@@ -1,7 +1,28 @@
 package ru.ssau.tk.faible.labs.io;
 
+import ru.ssau.tk.faible.labs.functions.Point;
+import ru.ssau.tk.faible.labs.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private  FunctionsIO(){
         throw new UnsupportedOperationException();
     }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        // записывваем количество точек
+        printWriter.println(function.getCount());
+
+        // записываем все точки функции
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
+
+
+    }
+
 }
