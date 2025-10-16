0cbbe4049e6ddbd916df5039b0dc27747cf54612
Sadrtdinov <nailsadrt8927@gmail.com>
2025-10-16 18:50:01 +0400

    В пакете concurrent создан класс MultiplyingTask, реализован метод Run

---
+++ src/main/java/ru/ssau/tk/faible/labs/concurrent/MultiplyingTask.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.faible.labs.concurrent;
+
+import ru.ssau.tk.faible.labs.functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable{
+
+    private final TabulatedFunction function; // приватное поле
+
+    // конструтор
+    private MultiplyingTask(TabulatedFunction function){
+        this.function = function;
+    }
+
+    @Override
+    public void run(){
+        // проходимся циклом по записям табулированной функции
+        for (int i = 0; i < function.getCount(); i++){
+            double currentY = function.getY(i);
+            // увеличиваем y в 2 раза
+            function.setY(i,currentY*2);
+        }
+        // выводим информацию в консоль о том, что поток закончил выполнение задачи
+        System.out.println("Поток " + Thread.currentThread().getName() +
+                "закончил выполнение задачи.");
+    }
+}
