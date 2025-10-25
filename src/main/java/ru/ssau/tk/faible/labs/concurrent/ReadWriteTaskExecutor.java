e02d8fdc9aabcee2dd0491149a672880b3a58dee
Aparkin <aparkin2017@mail.ru>
2025-10-26 00:41:59 +0400

    Добавлен класс ReadWriteTaskExecutor, запускающий 2 потока чтения и записи в табулированную функцию, вывод пока что может быть некорректным

---
+++ src/main/java/ru/ssau/tk/faible/labs/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.faible.labs.concurrent;
+
+import ru.ssau.tk.faible.labs.functions.ConstantFunction;
+import ru.ssau.tk.faible.labs.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.faible.labs.functions.TabulatedFunction;
+
+public class ReadWriteTaskExecutor {
+    public static void main(String[] args) {
+        ConstantFunction negativeFunction = new ConstantFunction(-1);
+        TabulatedFunction function = new LinkedListTabulatedFunction(negativeFunction, 1, 1000, 1000);
+
+        // создаем поток для ReadTask
+        Runnable readTask = new ReadTask(function);
+        Thread readThread = new Thread(readTask);
+
+        // создаем поток для WriteTask
+        Runnable writeTask = new WriteTask(0.5, function);
+        Thread writeThread = new Thread(writeTask);
+
+        // запускаем потоки
+        readThread.start();
+        writeThread.start();
+    }
+}
