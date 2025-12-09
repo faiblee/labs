f158a8520c4d80518923db550238848a71ddf3b0
Aparkin <aparkin2017@mail.ru>
2025-12-10 01:35:16 +0400

    Финальные изменения в настройки пользователя - работает

--- src/main/java/ru/ssau/tk/faible/labs/ui/dialogs/SettingsDialog.java
+++ src/main/java/ru/ssau/tk/faible/labs/ui/dialogs/SettingsDialog.java
@@ -6,28 +6,34 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.VaadinSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import ru.ssau.tk.faible.labs.ui.models.CurrentUser;
 import ru.ssau.tk.faible.labs.ui.utils.ExceptionHandler;
 import ru.ssau.tk.faible.labs.ui.utils.NotificationManager;
 
+import java.io.Serial;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class SettingsDialog extends Dialog {
 
+    @Serial
+    private static final long serialVersionUID = -3038251719462255126L;
     private final RestTemplate restTemplate = new RestTemplate();
     private final TextField usernameField = new TextField("Имя пользователя");
     private final PasswordField oldPasswordField = new PasswordField("Текущий пароль");
     private final PasswordField newPasswordField = new PasswordField("Новый пароль");
     private final PasswordField confirmPasswordField = new PasswordField("Подтвердите новый пароль");
+    private final Select<String> factorySelect = new Select<>();
 
     public SettingsDialog() {
         setWidth("500px");
@@ -41,13 +47,13 @@ public class SettingsDialog extends Dialog {
         }
         usernameField.setValue(currentUser.getUsername());
 
-        Select<String> factorySelect = new Select<>();
         factorySelect.setLabel("Фабрика функций");
         factorySelect.setItems("array factory", "linkedList factory");
+        log.info("Factory type: {}", currentUser.getFactory_type());
         factorySelect.setValue(currentUser.getFactory_type() + " factory");
 
         FormLayout form = new FormLayout();
-        form.add(usernameField, oldPasswordField, newPasswordField, confirmPasswordField);
+        form.add(usernameField, factorySelect, newPasswordField, confirmPasswordField);
         add(form);
 
         HorizontalLayout buttons = new HorizontalLayout();
@@ -59,21 +65,19 @@ public class SettingsDialog extends Dialog {
     }
 
     private void saveChanges(CurrentUser currentUser) {
-        // Создаём HttpEntity с заголовком Authorization
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Authorization", "Basic " + currentUser.getEncodedCredentials());
-//        HttpEntity<String> entity = new HttpEntity<>(headers);
-//        restTemplate.put()
         // Берем значения из форм
         String newUsername = usernameField.getValue();
         String oldPassword = oldPasswordField.getValue();
         String newPassword = newPasswordField.getValue();
         String confirmPassword = confirmPasswordField.getValue();
+        String factoryTypeFromSelect = factorySelect.getValue();
 
         if (!newPassword.equals(confirmPassword)) {
             ExceptionHandler.notifyUser(new IllegalArgumentException("Новые пароли не совпадают"));
             return;
         }
+
+        String factory_type = factoryTypeFromSelect.split(" ")[0]; // array или linkedList
 
         String url = "http://localhost:8080/api/users/" + currentUser.getId();
 
@@ -85,6 +89,9 @@ public class SettingsDialog extends Dialog {
         if (!newPassword.isEmpty() && !newPassword.equals(oldPassword)) {
             updateRequest.put("old_password", oldPassword);
             updateRequest.put("new_password", newPassword);
+        }
+        if (!factory_type.equals(currentUser.getFactory_type())) {
+            updateRequest.put("factory_type", factory_type);
         }
 
         if (updateRequest.isEmpty()) {
@@ -104,10 +111,24 @@ public class SettingsDialog extends Dialog {
             if (!newUsername.equals(currentUser.getUsername())) {
                 currentUser.setUsername(newUsername);
             }
-            // Обновляем encodedCredentials
-            String auth = currentUser.getUsername() + ":" + newPassword;
-            String encodedCredentials = Base64.getEncoder().encodeToString(auth.getBytes());
-            currentUser.setEncodedCredentials(encodedCredentials);
+            if (!factory_type.equals(currentUser.getFactory_type())) {
+                currentUser.setFactory_type(factory_type);
+            }
+
+            String credentials = new String(Base64.getDecoder().decode(currentUser.getEncodedCredentials()), StandardCharsets.UTF_8);
+            String[] parts = credentials.split(":", 2); // декодируем данные и разбиваем логин с паролем
+            String passwordFromHeader = parts[1];
+
+            if (newPassword.isEmpty() || oldPassword.isEmpty()) { // если меняли только username
+                // Обновляем encodedCredentials
+                String auth = currentUser.getUsername() + ":" + passwordFromHeader;
+                String encodedCredentials = Base64.getEncoder().encodeToString(auth.getBytes());
+                currentUser.setEncodedCredentials(encodedCredentials);
+            } else { // если меняли и пароль
+                String auth = currentUser.getUsername() + ":" + newPassword;
+                String encodedCredentials = Base64.getEncoder().encodeToString(auth.getBytes());
+                currentUser.setEncodedCredentials(encodedCredentials);
+            }
 
             NotificationManager.show("Изменения сохранены", 3000, Notification.Position.BOTTOM_CENTER);
         } catch (Exception ex) {
