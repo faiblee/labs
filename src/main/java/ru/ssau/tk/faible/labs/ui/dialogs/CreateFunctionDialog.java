43600da2fd0f769f69c242843e4dcbf74eecba39
Aparkin <aparkin2017@mail.ru>
2025-12-14 02:18:35 +0400

    Добавлено первое отображение графиков

--- src/main/java/ru/ssau/tk/faible/labs/ui/dialogs/CreateFunctionDialog.java
+++ src/main/java/ru/ssau/tk/faible/labs/ui/dialogs/CreateFunctionDialog.java
@@ -5,14 +5,19 @@ package ru.ssau.tk.faible.labs.ui.dialogs;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.IntegerField; // Используем IntegerField для количества точек
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Setter;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.function.ValueProvider;
 import com.vaadin.flow.server.VaadinSession;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -22,6 +27,10 @@ import ru.ssau.tk.faible.labs.ui.models.CurrentUser;
 import ru.ssau.tk.faible.labs.ui.models.CreateFunctionDTO;
 import ru.ssau.tk.faible.labs.ui.utils.ExceptionHandler;
 import ru.ssau.tk.faible.labs.ui.utils.NotificationManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class CreateFunctionDialog extends Dialog {
 
@@ -63,8 +72,6 @@ public class CreateFunctionDialog extends Dialog {
 
         // Настройка поля ввода константы
         constantField.setVisible(false);
-
-        // Настройка полей XFrom, XTo, Count
         xFromField.setVisible(false);
         xToField.setVisible(false);
         countField.setVisible(false);
@@ -94,7 +101,6 @@ public class CreateFunctionDialog extends Dialog {
                 xFromField.focus(); // (опционально) перевести фокус на первое поле
             }
         });
-
         // Форма
         FormLayout form = new FormLayout();
         form.add(title, description, nameField, typeSelect, constantField, xFromField, xToField, countField);
