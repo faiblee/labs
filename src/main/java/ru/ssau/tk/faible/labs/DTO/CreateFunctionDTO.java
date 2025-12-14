722843bb16edd9e7b2257bdb8685c7f7fe841f97
Aparkin <aparkin2017@mail.ru>
2025-12-14 23:16:59 +0400

    Добавлена поддержка добавления функции поточечно

--- src/main/java/ru/ssau/tk/faible/labs/DTO/CreateFunctionDTO.java
+++ src/main/java/ru/ssau/tk/faible/labs/DTO/CreateFunctionDTO.java
@@ -3,14 +3,9 @@ package ru.ssau.tk.faible.labs.DTO;
 public class CreateFunctionDTO {
     private String name;
     private int ownerId;
-    private String type;
-    private double xfrom;
-    private double xto;
-    private int count;
-    private double constant;
-    private String factory_type;
-    private double[] xvalues;
-    private double[] yvalues;
+
+    public CreateFunctionDTO() {
+    }
 
     public CreateFunctionDTO(String name, int ownerId, String type, double xfrom, double xto, int count, double constant, String factory_type, double[] xvalues, double[] yvalues) {
         this.name = name;
@@ -25,37 +20,7 @@ public class CreateFunctionDTO {
         this.yvalues = yvalues;
     }
 
-    public double[] getYvalues() {
-        return yvalues;
-    }
-
-    public void setYvalues(double[] yvalues) {
-        this.yvalues = yvalues;
-    }
-
-    public double[] getXvalues() {
-        return xvalues;
-    }
-
-    public void setXvalues(double[] xvalues) {
-        this.xvalues = xvalues;
-    }
-
-    public double getXto() {
-        return xto;
-    }
-
-    public void setXto(double xto) {
-        this.xto = xto;
-    }
-
-    public double getXfrom() {
-        return xfrom;
-    }
-
-    public void setXfrom(double xfrom) {
-        this.xfrom = xfrom;
-    }
+    private String type;
 
     public String getName() {
         return name;
@@ -69,8 +34,8 @@ public class CreateFunctionDTO {
         return ownerId;
     }
 
-    public void setOwnerId(int owner_id) {
-        this.ownerId = owner_id;
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
     }
 
     public String getType() {
@@ -81,19 +46,19 @@ public class CreateFunctionDTO {
         this.type = type;
     }
 
-    public double getxfrom() {
+    public double getXfrom() {
         return xfrom;
     }
 
-    public void setxfrom(double xfrom) {
+    public void setXfrom(double xfrom) {
         this.xfrom = xfrom;
     }
 
-    public double getxto() {
+    public double getXto() {
         return xto;
     }
 
-    public void setxto(double xto) {
+    public void setXto(double xto) {
         this.xto = xto;
     }
 
@@ -120,4 +85,28 @@ public class CreateFunctionDTO {
     public void setFactory_type(String factory_type) {
         this.factory_type = factory_type;
     }
+
+    public double[] getXvalues() {
+        return xvalues;
+    }
+
+    public void setXvalues(double[] xvalues) {
+        this.xvalues = xvalues;
+    }
+
+    public double[] getYvalues() {
+        return yvalues;
+    }
+
+    public void setYvalues(double[] yvalues) {
+        this.yvalues = yvalues;
+    }
+
+    private double xfrom;
+    private double xto;
+    private int count;
+    private double constant;
+    private String factory_type;
+    private double[] xvalues;
+    private double[] yvalues;
 }
